b51cf2ab71e680ee6cf6de5a26c9d5a9d92ee026
Douglas-Edson <douglas22@live.com.pt>
2025-12-05 20:35:06 -0300

    Primeiro commit - início curso Java10X

---
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,17 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+         * Dados primitivos - int, double, float, char, boolean, short.
+         * Objetivo da aula: Criar um ninja - Naruto -
+         */
+
+       int idade = 16;
+       double altura = 1.65;
+       char inicial = 'N';
+       boolean vivoOuMorto = true;
+       Long saldoBancario = 9999999L ;
+       String nome = "Naruto";
+    }
+}
